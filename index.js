fed6a935253bf405db78380356c8b2e6f1768a0f
Dean Spooner <dean.spooner@me.com>
2021-04-09 22:40:12 +0100

    Added fade-out for starting elements and added rail, train and platform after.

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
-document.getElementById('startButton').addEventListener("click", function () {
-    let title = document.getElementById('title');
-    title.style.opacity = '0';
-    this.parentElement.removeChild(title);
-});
+let title = document.getElementById('title');
+let button = document.getElementById('startButton');
+let viewport = document.getElementById('viewport');
+let tracks = document.createElement('img');
+tracks.id = 'tracks';
+tracks.src = 'images/tracks.png';
+let train = document.createElement('img');
+train.id = 'train';
+train.src = 'images/train.png';
+let platform = document.createElement('img');
+platform.id = 'platform';
+platform.src = 'images/platform.png';
+
+swapElements = () => {
+    title.classList.toggle('fadeOut');
+    button.classList.toggle('fadeOut');
+    setTimeout(function () {
+        title.remove();
+        button.remove();
+    }, 600);
+    viewport.appendChild(tracks);
+    viewport.appendChild(platform);
+    viewport.appendChild(train);
+}
+
+
+
+document.getElementById('startButton').addEventListener("click", swapElements);
